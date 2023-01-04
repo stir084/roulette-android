e61478976f8e8c9628cf5824863d996c194ae994
junho <wnsgh1357@gmail.com>
2023-01-05 01:39:16 +0900

    공유하기 기능 완료

--- app/src/main/java/com/example/roulette_together/MainActivity.kt
+++ app/src/main/java/com/example/roulette_together/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.roulette_together
 
 import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.core.content.ContextCompat.startActivity
+import java.net.URISyntaxException
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,11 +22,57 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val myWebView: WebView = findViewById(R.id.webView)
-        myWebView.webViewClient = MyWebViewClient() //하단 앱 공유하기 기능 연결
-        myWebView.settings.javaScriptEnabled = true // 자바 스크립트 허용
+
+        myWebView.settings.run {
+            // 웹뷰 자바스크립트 허용
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(true)
+        }
+
+        myWebView.webViewClient = object: WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView,request: WebResourceRequest): Boolean {
+                Log.d(TAG, request.url.toString())
+
+                if (request.url.scheme == "intent") {
+                    try {
+                        // Intent 생성
+                        val intent = Intent.parseUri(request.url.toString(), Intent.URI_INTENT_SCHEME)
+
+                        // 실행 가능한 앱이 있으면 앱 실행
+                      //  if (intent.resolveActivity(packageManager) != null) {
+                            startActivity(intent)
+                            Log.d(TAG, "ACTIVITY: ${intent.`package`}")
+                            Log.d(TAG, "카카오톡 실행")
+                            return true
+                      //  }
+                        // 실행 못하는 경우 카카오톡 마켓으로 이동해야 하지만 resolveActivity가 제대로 작동하지 않아서
+                        // 기능 빼버림.
+                        Log.d(TAG, "카카오톡 공유하기 실행 못함")
+
+                        // 실행 못하면 웹뷰는 카카오톡 공유하기 화면으로 이동
+                        // myWebView.loadUrl("http://kakao-share.s3-website.ap-northeast-2.amazonaws.com/")
+
+                        // 구글 플레이 카카오톡 마켓으로 이동
+                      /*  val intentStore = Intent(Intent.ACTION_VIEW)
+                        intentStore.addCategory(Intent.CATEGORY_DEFAULT)
+                        intentStore.data = Uri.parse("market://details?id=com.kakao.talk")
+                        Log.d(TAG, "구글 플레이 카카오톡 마켓으로 이동")
+                        startActivity(intentStore)*/
+
+                    } catch (e: URISyntaxException) {
+                        Log.e(TAG, "!!! 에러 Invalid intent request", e)
+                    }
+                }
+                // 나머지 서비스 로직 구현
+                return false
+            }
+        } //하단 앱 공유하기 기능 연결
+        ///myWebView.settings.javaScriptEnabled = true // 자바 스크립트 허용
         /* 웹뷰에서 새 창이 뜨지 않도록 방지하는 구문 */
-        myWebView.webViewClient = WebViewClient()
-        myWebView.webChromeClient = WebChromeClient()
+       // myWebView.webViewClient = WebViewClient()
+       // myWebView.webChromeClient = WebChromeClient()
         myWebView.loadUrl("http://116.37.136.233:5000")
 
 
@@ -34,47 +85,6 @@ class MainActivity : AppCompatActivity() {
             myWebView.goBack()
         }else{
             super.onBackPressed()
-        }
-    }
-
-    private class MyWebViewClient : WebViewClient() {
-        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            return if (url.startsWith(INTENT_PROTOCOL_START)) {
-                val customUrlStartIndex = INTENT_PROTOCOL_START.length
-                val customUrlEndIndex = url.indexOf(INTENT_PROTOCOL_INTENT)
-                if (customUrlEndIndex < 0) {
-                    false
-                } else {
-                    val customUrl = url.substring(customUrlStartIndex, customUrlEndIndex)
-                    try {
-                        view.getContext().startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(customUrl)))
-                    } catch (e: ActivityNotFoundException) {
-                        val packageStartIndex = customUrlEndIndex + INTENT_PROTOCOL_INTENT.length
-                        val packageEndIndex = url.indexOf(INTENT_PROTOCOL_END)
-                        val packageName = url.substring(
-                            packageStartIndex,
-                            if (packageEndIndex < 0) url.length else packageEndIndex
-                        )
-                        view.getContext().startActivity(
-                            Intent(
-                                Intent.ACTION_VIEW, Uri.parse(
-                                    GOOGLE_PLAY_STORE_PREFIX + packageName
-                                )
-                            )
-                        )
-                    }
-                    true
-                }
-            } else {
-                false
-            }
-        }
-
-        companion object {
-            const val INTENT_PROTOCOL_START = "intent:"
-            const val INTENT_PROTOCOL_INTENT = "#Intent;"
-            const val INTENT_PROTOCOL_END = ";end;"
-            const val GOOGLE_PLAY_STORE_PREFIX = "market://details?id="
         }
     }
 }
